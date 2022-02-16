52c7e23f21ae31d2865ebd859c21e5d46b626b55
hilda1737 <hildamoraahh@gmail.com>
2022-02-16 22:14:39 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main () {
+    var name:String = "Hilda Moraa"
+    var phoneNumber:String = "0793769696"
+    var weight:String = "70kg"
+    var age:String = "25"
+    var iskenyan : Boolean = false
+    println(name)
+    println(iskenyan)
+
+
+
+
+
+
+
+
+
+}
